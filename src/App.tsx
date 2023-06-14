45a116ab406fe3de363d2368d81634b28f6ae446
ATEOTG <bryansantos9797@gmail.com>
2023-06-14 12:11:11 -0400

    added routes for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import "./scss/main.scss";
 import Login from "./components/Login";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate replace to="/user/login" />} />
         <Route path="/user/*" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   );
