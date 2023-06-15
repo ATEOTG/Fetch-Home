e02e81a016c5d80641ffe2bdb2ae487a2398dca3
ATEOTG <bryansantos9797@gmail.com>
2023-06-14 22:42:29 -0400

    changed div to fragment

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,17 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import "./scss/main.scss";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import { Fragment } from "react";
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Routes>
         <Route path="/" element={<Navigate replace to="/user/login" />} />
         <Route path="/user/*" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
-    </div>
+    </Fragment>
   );
 }
 
